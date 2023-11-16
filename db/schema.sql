7a2ee71e81a21fba33dc5865ab7407f3677de1ce
lgxxhi <luisgsilva95@gmail.com>
2023-11-16 10:50:53 -0500

    add reviews table to dev

--- db/schema.sql
+++ db/schema.sql
@@ -4,43 +4,10 @@ CREATE DATABASE prorated_dev;
 
 \c prorated_dev;
 
-DROP TABLE IF EXISTS contractors;
-
-CREATE TABLE contractors (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    description TEXT NOT NULL,
-    ratings INT,
-    reviews INT
-);
-
-
-DROP TABLE IF EXISTS contractors_services;
-
-CREATE TABLE contractors_services (
-    id SERIAL PRIMARY KEY,
-    contractor_id INTEGER REFERENCES contractors(id),
-    service_id INTEGER REFERENCES services(id) 
-);
-
-DROP TABLE IF EXISTS services;
-
-CREATE TABLE services (
-    id SERIAL PRIMARY KEY, 
-    name VARCHAR(50)
-);
-
-
-DROP TABLE IF EXISTS images;
-
-CREATE TABLE images (
-    id SERIAL PRIMARY KEY,
-    contractor_id INTEGER REFERENCES contractors(id),
-    image_url VARCHAR(255) NOT NULL
-);
-
-
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS contractors_services;
+DROP TABLE IF EXISTS contractors;
+DROP TABLE IF EXISTS services;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -54,3 +21,26 @@ CREATE TABLE users (
   location VARCHAR(100)
 );
 
+CREATE TABLE contractors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    description TEXT NOT NULL,
+    ratings INT,
+    reviews INT
+);
+
+CREATE TABLE services (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    description TEXT NOT NULL,
+    custom BOOLEAN,
+    image TEXT
+);
+
+CREATE TABLE contractors_services (
+    id SERIAL PRIMARY KEY,
+    contractor_id INTEGER REFERENCES contractors(id)
+    ON DELETE CASCADE,
+    service_id INTEGER REFERENCES services(id)
+    ON DELETE CASCADE
+);
--- db/seed.sql
+++ db/seed.sql
@@ -9,27 +9,71 @@ VALUES
   ('sarah_white', '12345678', 'sarah.white@example.com', 'Sarah', 'White', '555-234-5678', 'https://cloudfront-us-east-2.images.arcpublishing.com/reuters/EHAIKCVG6NLBNNVXZE7AXPAAJ4.jpg', 'San Francisco'),
   ('mike_jackson', 'p@ssw0rd', 'mike.jackson@example.com', 'Mike', 'Jackson', '555-876-1234', 'https://celebdial.s3.amazonaws.com/celeb_pic/michael-jackson.jpg', 'Miami');
 
-INSERT INTO contractors (name, description, ratings, reviews) VALUES
-  ('Aundale', 'Experienced contractor in Electrician', 4, 20),
-  ('Michael', 'Skilled in service 2 and Plumbing', 3, 15),
-  ('Luis', 'Specializes in service 1 and Landscaping', 5, 30);
+INSERT INTO
+  contractors(name, description, ratings, reviews)
+VALUES
+  ('Smith Construction', 'General construction services for residential and commercial projects', 4.8, 35),
+  ('GreenScape Landscaping', 'Specializing in sustainable and creative landscaping solutions', 4.5, 28),
+  ('Swift Roofing Experts', 'Swift and reliable roofing services with attention to detail', 4.9, 42),
+  ('PlumbMasters', 'Your go-to experts for all plumbing needs, from repairs to installations', 4.7, 39),
+  ('Wired Wizards', 'Electrical wizards providing safe and efficient wiring solutions', 4.6, 31),
+  ('ColorSplash Painters', 'Transforming spaces with a splash of colors and professional painting', 4.9, 47),
+  ('FloorCrafters', 'Crafting beautiful and durable floors with precision and expertise', 4.8, 36),
+  ('CoolBreeze HVAC', 'Keeping your home comfortable with top-notch HVAC maintenance', 4.7, 40),
+  ('BugBusters Pest Control', 'Busting bugs and pests to keep your home and business pest-free', 4.6, 29),
+  ('MasterCrafter Carpentry', 'Masterful carpentry and woodwork for unique and custom projects', 4.8, 38),
+  ('IronGuard Fencing', 'Providing strong and aesthetically pleasing fence installations', 4.7, 33),
+  ('CrystalClear Windows', 'Experts in professional window replacement for energy-efficient homes', 4.9, 45),
+  ('ApplianceSavers', 'Saving your appliances with quick and reliable repair services', 4.5, 27),
+  ('BathBliss Remodeling', 'Bringing bliss to your bathroom with transformative remodeling', 4.8, 41),
+  ('SecureHaven Security', 'Creating secure havens with advanced home security installations', 4.9, 50),
+  ('PowerSpark Electricians', 'Powerful solutions for all your electrical needs', 4.7, 37);
 
 
-INSERT INTO services (name) VALUES
-  ('Electrician'),
-  ('Plumbing'),
-  ('Carpentry'),
-  ('Landscaping');
+INSERT INTO 
+  services(name, description, custom, image)
+VALUES
+  ('Home Renovation', 'Complete renovation services for residential properties', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Landscaping Design', 'Customized landscaping design and implementation', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Roof Repair', 'Professional roof repair and maintenance', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Plumbing Services', 'Comprehensive plumbing solutions for homes and businesses', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Electrical Wiring', 'Safe and efficient electrical wiring installation and repair', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Painting Services', 'Interior and exterior painting services with a variety of color options', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Flooring Installation', 'High-quality flooring installation for different types of flooring', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('HVAC Maintenance', 'Regular maintenance and repair of heating, ventilation, and air conditioning systems', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Pest Control', 'Effective pest control services for homes and commercial spaces', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Carpentry and Woodwork', 'Custom carpentry and woodwork for various projects', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Fence Installation', 'Durable and aesthetically pleasing fence installation', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Window Replacement', 'Professional window replacement services for improved energy efficiency', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Appliance Repair', 'Quick and reliable repair services for household appliances', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Bathroom Remodeling', 'Transformative bathroom remodeling with modern designs', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Home Security Installation', 'Installation of advanced home security systems for enhanced safety', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg'),
+  ('Electrician', 'Skilled electricians for all your electrical needs', FALSE , 'https://cdn1.vectorstock.com/i/1000x1000/32/95/isometric-construction-vertical-composition-vector-18283295.jpg');
 
-INSERT INTO contractors_services (contractor_id, service_id) VALUES
-  (1, 1), -- Contractor1 offers Service1
-  (2, 2), -- Contractor2 offers Service2
-  (2, 3), -- Contractor2 also offers Service3
-  (3, 1), -- Contractor3 offers Service1
-  (3, 4); -- Contractor3 offers Service4
 
-  INSERT INTO images (contractor_id, image_url) VALUES
-    (1, "https://conductiveelectric.com/wp-content/uploads/2015/09/electricianworkingelectricalpanel.jpg"),
-    (2, 'https://electricianridgewood.com/wp-content/uploads/2018/04/Wiring-New-Construction.jpg'),
-  
+INSERT INTO
+  contractors_services (contractor_id, service_id)
+VALUES
+  (1, 1),
+  (1, 3),
+  (2, 2),
+  (2, 10),
+  (3, 4),
+  (3, 11),
+  (4, 5),
+  (4, 14),
+  (5, 6),
+  (5, 15),
+  (6, 7),
+  (6, 8),
+  (7, 9),
+  (7, 12),
+  (8, 13),
+  (8, 16),
+  (9, 12),
+  (9, 16),
+  (10, 16),
+  (10, 16);
 
+
+INSERT INTO reviews (contractor_id, user_id, review, rating) VALUES (1, 2,  'restaurant was awesome', 5, '2021-08-19');
