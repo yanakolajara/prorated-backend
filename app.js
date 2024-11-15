c0aedf1896d5f977b3d7b585f1231f01178f1b4d
yanakolajara <yanakolajara@pursuit.org>
2024-11-14 19:48:39 -0500

    console logs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
+console.log('app.js: START');
 const express = require('express');
-
+console.log('app.js 1: const express = require("express");');
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -11,14 +12,18 @@ const serviceController = require('./controllers/serviceController');
 const projectListingsController = require('./controllers/projectListingsController');
 const reviewsController = require('./controllers/reviewsController');
 
+console.log(
+  'app.js 2: const userController = require("./controllers/userController");'
+);
 // CONFIG
 const app = express();
+console.log('app.js 3: const app = express();');
 
 // MIDDLEWARE
 app.use(morgan('dev')); // Log HTTP requests
 app.use(express.json()); // Parse incoming JSON
 app.use(cors()); // Enable Cross Origin Resource Sharing
-
+console.log('app.js 4: app.use(morgan("dev"));');
 // ROUTES
 app.use('/users', userController);
 app.use('/contractors', contractorController);
